4c3c1165c87e0e47d9c0f04e7665c8413bdd10fb
tanishqsonwane17 <tanishqsonwane32@gmail.com>
2026-04-29 12:16:17 +0530

    leftRotationByK

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let arr = [1,2,3,4,5]
-const num = arr[arr.length-1]
+let k = 2
 
-for(let i = arr.length-2; i>=0; i--){
-    arr[i+1] = arr[i]
+
+for(let j =0; j<k; j++){
+    let temp = arr[0]
+    for(let i = 1; i<arr.length; i++){
+    arr[i-1] = arr[i]
 }
-arr[0] = num
+
+arr[arr.length-1] = temp
+}
 
 console.log(arr)
