bce990373bcdaa37865b12e59615df24c1ce2950
tanishqsonwane17 <tanishqsonwane32@gmail.com>
2026-05-09 13:39:10 +0530

    Remove Duplicates From the Sorted Array

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
-let arr1 = [1,3,5,7,9]
-let arr2 = [2,4,6,8,10]
-let newArr = new Array( arr1.length + arr2.length )
-let i = 0;
-let j = 0;
-let k = 0
+let arr = [0,0,1,1,1,2,2,3,3,4]
+let i = 0
+let j = 1
 
-while(arr1[i] < arr1.length && arr2[j]<arr2.length){
-  if(arr1[i] < arr2[j]){
-    newArr[k++] = arr1[i++]
+while(i<arr.length-1){
+  if(arr[i] != arr[i+1]){
+    arr[j] = arr[i+1]
+    
+    j++
   }
-  else{
-    newArr[k++] = arr2[j++]
-  }
+  i++
 }
 
-while(i<arr1.length){
-  newArr[k++] = arr1[i++]
-}
-while(j<arr2.length){
-  newArr[k++] = arr2[j++]
-}
-
-console.log(newArr)
+console.log(arr)
