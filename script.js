70d7190367c7d63d7c406abca4d4dd322a0adf9d
tanishqsonwane17 <tanishqsonwane32@gmail.com>
2026-05-09 19:10:52 +0530

    Move Zerors

--- script.js
+++ script.js
@@ -1,33 +1,17 @@
-let arr = [1,0,2,3,0,4,5]
-let countZero = 0
+let arr = [0,1,0,3,12]
+let j = 0;
+let i = 0;
 
-for(let i = 0; i < arr.length; i++){
-  if(arr[i] == 0){
-    countZero++
-  }
-}
-
-let i = arr.length - 1
-let j = arr.length - 1 + countZero
-
-while(i != j){
-
-  if(j < arr.length){
-    arr[j] = arr[i]
+while(i<arr.length){
+ 
+  if(arr[i] !=0 ){
+    let temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+    j++
   }
 
-  j--
-
-  if(arr[i] == 0){
-
-    if(j < arr.length){
-      arr[j] = arr[i]
-    }
-
-    j--
-  }
-
-  i--
+  i++
 }
 
 console.log(arr)
