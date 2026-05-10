25b34e87cdfbe87f96b12c58d5558b1e5362f45f
tanishqsonwane17 <tanishqsonwane32@gmail.com>
2026-05-10 11:27:48 +0530

    Maximum Sub Array

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-let arr = [0,1,0,3,12]
-let j = 0;
-let i = 0;
+let arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+let sum = 0;
+let maxSum = arr[0]
+let i = 0
 
 while(i<arr.length){
- 
-  if(arr[i] !=0 ){
-    [arr[i], arr[j]] = [arr[j], arr[i]]
-    j++
+  sum += arr[i]
+  if(sum>maxSum){
+    maxSum = sum
   }
-
+  if(sum<0){
+    sum = 0
+  }
   i++
 }
 
-console.log(arr)
+console.log(maxSum)
