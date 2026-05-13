858020435c0884362d677a176baa2de14c0e6c79
tanishqsonwane17 <tanishqsonwane32@gmail.com>
2026-05-13 15:08:48 +0530

    Set Colours

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-let arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-let sum = 0;
-let maxSum = arr[0]
+let arr = [1,1,0,1,0,2,1,0,1,2,1,0]
 let i = 0
+let j = 0
+let k = arr.length-1
 
-while(i<arr.length){
-  sum += arr[i]
-  if(sum>maxSum){
-    maxSum = sum
+while(i<=k){
+  if(arr[i] == 0){
+    let temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+    j++
+    i++
   }
-  if(sum<0){
-    sum = 0
+  if(arr[i] == 2){
+    let temp = arr[i]
+    arr[i] = arr[k]
+    arr[k] = temp
+    k--
+
+    if(arr[i] == 0){
+      let temp = arr[i]
+      arr[i] = arr[j]
+      arr[j] = temp
+      i++
+      j++
+    }
   }
-  i++
+  else{
+    i++
+  }
 }
 
-console.log(maxSum)
+console.log(arr)
