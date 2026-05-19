67c0df195647a2794ac20e28cb44b6b29351505c
tanishqsonwane17 <tanishqsonwane32@gmail.com>
2026-05-19 07:44:24 +0530

    Insertion Soort

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
-let arr = [2,5,8,13,16,22,25,29,33,34,69,70]
-let i = 0
-let j = arr.length-1
-let mid = 0
-let target = 33
-let ans = 0
-while(i<=j){
-  mid = Math.floor((i + j) / 2)
-  if(arr[mid] == target){
-    ans =  `you elem is presnet on ${mid} index`
-    break
-  }
-  if(arr[mid] < target){
-    i = mid+1
-    ans = `you elem is presnet on ${mid} index`
-  }else{
-    j = mid-1
-    ans  = `you elem is presnet on ${mid} index`
-  }
-}
+const arr = [8,2,1,5,12,9,27,16]
 
-console.log(ans)
+for(let i = 0; i<arr.length; i++){
+    let key = arr[i]
+    let j = i-1
+
+    while(j>=0 &&  key<=arr[j]){
+        arr[j+1] = arr[j]
+        j--
+    }
+    arr[j+1] = key 
+}
+console.log(arr)
